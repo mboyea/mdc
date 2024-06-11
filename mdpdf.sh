f1545770d33743059dd1971b46f845088c35042b
U-AAONINC\matthew.boyea <matthew.boyea@PV-8SMG5Y3-CTRL.AAONINC.COM>
2024-06-10 21:51:52 -0500

    initial commit

---
+++ mdpdf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+target_dir=pwd
+# script_dir=dirname "$(readlink -f "$0")"
+
+# TODO test pandoc, pdflatex, watchexec
+
+print_help() {
+	echo "Use Pandoc to convert Markdown+LaTeX to PDF"
+	echo "mdpdf [OPTION]... [FILE]..."
+	echo
+	echo "OPTIONS:"
+	echo "  -h        --help"
+	echo "  -w        --watch"
+}
+
+# bash --init-file <(cd $script_dir && make -C $target_dir)
+
+# if not matched with any action option, print help
+print_help
